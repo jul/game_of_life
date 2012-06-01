d6633ab37ba2bcf45f394b99b38aefd5ef7ba296
julien tayon <jtayon@gmail.com>
2012-06-01 19:58:45 +0200

    pti bug

--- gof.dsl.py
+++ gof.dsl.py
@@ -3,12 +3,13 @@ from util.matrix import matrix
 import os
 from time import sleep
 
-X = 16
-Y = 14
+X = 22
+Y = 60
 DEAD = 0
 ALIVE = 1
 
 grid = matrix(X, Y, [DEAD]* X*Y)
+speed=1.0
 
 oscillator = [(0,0),(0,1),(0,2)]
 still = [(0,0),(0,1),(1,0),(1,1)]
@@ -27,21 +28,23 @@ time=0
 def play(times=1):
     global grid
     global time
+    global speed
     while times:
         os.system('clear')
         times-=1
         time+=1
         print grid
         print "time:<%3d>" % time
-        sleep(1)
+        sleep(1.0/speed)
         n_grid = grid.copy()
-        for x in range(X):
-            for y in range(Y):
+        for x in range(grid.size_x):
+            for y in range(grid.size_y):
                 if grid.get(x, y):
                     n_grid.set(x, y, grid.nb_living_around(x, y) in [2, 3])
                 else:
                     n_grid.set(x, y, grid.nb_living_around(x, y) == 3)
         grid = n_grid
+
 def reset(x=X,y=Y):
     global grid
     global time
