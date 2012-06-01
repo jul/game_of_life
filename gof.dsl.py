fda08741b6e3f4232a398f37bc75f5452258ebec
julien tayon <jtayon@gmail.com>
2012-06-01 17:24:25 +0200

    a version usable in an ipython console : reset(X,Y) to reset the grid and time print grid to show the grid at(X,Y, glider or fix or oscillator) to set a sprite at given pos play(times=1) to see the grid evolve n_grid = grid.copy() to take a snapshot of the grid grid.set(X,Y,DEAD or ALIVE) to set the state of the grid at the given position grid.set(X,Y) to get the state grid.matrix to have the underlying object used

---
+++ gof.dsl.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+from util.matrix import matrix
+import os
+from time import sleep
+
+X = 16
+Y = 14
+DEAD = 0
+ALIVE = 1
+
+grid = matrix(X, Y, [DEAD]* X*Y)
+
+oscillator = [(0,0),(0,1),(0,2)]
+still = [(0,0),(0,1),(1,0),(1,1)]
+glider = [(0,2),(1,2),(2,2),(2,1),(0,0)]
+
+def at(x,y, pattern):
+    global grid
+    for dx,dy in pattern:
+        grid.set(x+dx,y+dy,ALIVE)
+
+at(1,8, glider)
+at(2,3, oscillator)
+at(6,5, still)
+
+time=0
+def play(times=1):
+    global grid
+    global time
+    while times:
+        os.system('clear')
+        times-=1
+        time+=1
+        print grid
+        print "time:<%3d>" % time
+        sleep(1)
+        n_grid = grid.copy()
+        for x in range(X):
+            for y in range(Y):
+                if grid.get(x, y):
+                    n_grid.set(x, y, grid.nb_living_around(x, y) in [2, 3])
+                else:
+                    n_grid.set(x, y, grid.nb_living_around(x, y) == 3)
+        grid = n_grid
+def reset(x=X,y=Y):
+    global grid
+    global time
+    time = 0
+    grid = matrix(x, y, [DEAD]*x*y)
